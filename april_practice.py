b35581789ff4dbaf0be23223014378e12adf4249
dmithamo <dennisbmithamo@gmail.com>
2018-04-08 09:40:50 +0300

    Codewars: 3 - Retrieve median character(s) of a given string

--- april_practice.py
+++ april_practice.py
@@ -1,4 +1,6 @@
 """
+1.
+
 Usually when you buy something, you're asked whether your 
 credit card number, phone number or answer to 
 your most secret question is still correct. However, since someone 
@@ -14,5 +16,22 @@ def maskify(cc):
     
     return "{}{}".format("#"*len(cc[4:]), cc[-4:])
 
-cc = "1111255542011dxennis"
-print(maskify(cc))
+"""
+2. 
+
+You are going to be given a word. 
+Your job is to return the middle character of the word. 
+If the word's length is odd, return the middle character. 
+If the word's length is even, return the middle 2 characters.
+"""
+
+def get_middle(s):
+    if len(s) % 2 == 0:
+        return "{}{}".format(s[len(s)//2-1], s[len(s)//2])
+    else:
+        return "{}".format(s[len(s)//2])
+
+print(get_middle("testing"))
+print(get_middle("middle"))
+print(get_middle("of"))
+         
